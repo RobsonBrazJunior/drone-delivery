66922658d5f1369aa9ce6fd2ef5259c38076bd6d
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-10-09 19:47:52 -0300

    Configure dependency injection

--- DroneDelivery.WebApi/Program.cs
+++ DroneDelivery.WebApi/Program.cs
@@ -1,11 +1,18 @@
+using DroneDelivery.Domain.Interfaces;
+using DroneDelivery.WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IGraphService, GraphService>();
+builder.Services.AddScoped<IDeliveryService, DeliveryService>();
 
 var app = builder.Build();
 
