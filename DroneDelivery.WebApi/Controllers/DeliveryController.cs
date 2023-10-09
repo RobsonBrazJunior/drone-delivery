218ea4f8e6e7c97491412679c9420d23da0c6efb
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-10-09 19:47:20 -0300

    Create DeliveryController

---
+++ DroneDelivery.WebApi/Controllers/DeliveryController.cs
@@ -0,0 +1,24 @@
+﻿using DroneDelivery.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DroneDelivery.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DeliveryController : ControllerBase
+	{
+		private readonly IDeliveryService _deliveryService;
+
+		public DeliveryController(IDeliveryService deliveryService)
+		{
+			_deliveryService = deliveryService ?? throw new ArgumentNullException(nameof(deliveryService));
+		}
+
+		[HttpPost("delivery-package")]
+		public async Task<IActionResult> DeliveryPackage(IList<string> entries)
+		{
+			var result = await _deliveryService.DeliveryPackage(entries);
+			return Ok(result);
+		}
+	}
+}
